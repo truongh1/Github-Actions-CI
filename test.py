1538cd5536676021e2fdf192f02b2a39bbfc27bc
Hao Truong <truongh@oregonstate.edu>
2021-06-05 15:35:50 -0700

    fixed lint errors

--- test.py
+++ test.py
@@ -7,7 +7,6 @@ class TestCase(unittest.TestCase):
     def test_add_1(self):
         self.assertEqual(example.add(1, 2), 3)
 
-
     def test_subtract_1(self):
         self.assertEqual(example.subtract(1, 1), 0)
 
