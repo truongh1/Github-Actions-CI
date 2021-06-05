e37b9960452735074a8c3ef736538fd528d153bc
Hao Truong <truongh@oregonstate.edu>
2021-06-05 15:33:45 -0700

    added test for subtraction

--- test.py
+++ test.py
@@ -8,5 +8,9 @@ class TestCase(unittest.TestCase):
         self.assertEqual(example.add(1, 2), 3)
 
 
+    def test_subtract_1(self):
+        self.assertEqual(example.subtract(1, 1), 0)
+
+
 if __name__ == '__main__':
     unittest.main()
