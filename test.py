56b6bf41840b9727a6bbdfee87cedd95f3522b7d
Hao Truong <truongh@oregonstate.edu>
2021-06-05 15:40:38 -0700

    added test for multiplication

--- test.py
+++ test.py
@@ -10,6 +10,9 @@ class TestCase(unittest.TestCase):
     def test_subtract_1(self):
         self.assertEqual(example.subtract(1, 1), 0)
 
+    def test_multiply_1(self):
+        self.assertEqual(example.multiply(2, 2), 4)
+
 
 if __name__ == '__main__':
     unittest.main()
